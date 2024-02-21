12ab40c63551bf85f3d52d8dffb08552f863761d
Sumit Kumar <108218318+iamsk10@users.noreply.github.com>
2024-02-21 10:55:40 +0530

    Update 02 Qr_generator.py

--- 02 Qr_generator.py
+++ 02 Qr_generator.py
@@ -10,4 +10,5 @@ data = input('enter your message to convert qr : ')
 qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill="black", back_color = "white")
+# save the qr in image formate
 img.save("twono.png")
